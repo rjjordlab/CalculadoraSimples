af50755f695521654321fbbb69efea777e10ba36
USER01 <USER01@LAPPAD>
2018-03-08 18:54:51 +0000

    Programa completo da calc simples

---
+++ CalculadoraSimples/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculadoraSimples.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Home
+        [HttpPost]
+        public ActionResult Index(int operando1=0, int operando2=0, string operador="")
+        {
+            int resultado = 0; //variavel AUX
+            switch (operador)  //Calc resultado
+            {
+                case "+": resultado = operando1 + operando2; break; //soma
+                case "-": resultado = operando1 - operando2; break; //sub
+                case "x": resultado = operando1 * operando2; break; //mult
+                case ":": resultado = operando1 / operando2; break; //div
+            }
+            ViewBag.Operando1 = operando1;
+            ViewBag.Operador = operador;
+            ViewBag.Operando2 = operando2;
+            ViewBag.Resultado = resultado; //entregar os dados a view            
+            return View(); //invocar a view
+        }
+    }
+}
